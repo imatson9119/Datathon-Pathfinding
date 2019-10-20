1eb052d10fb6bef93f2926629dc283b6fc4becf8
Ian Matson <imatson9119@gmail.com>
2019-10-20 07:49:33 -0500

    heap implementation

--- astar2.py
+++ astar2.py
@@ -1,75 +1,135 @@
-import numpy
+import numpy as np
+from time import sleep
 from heapq import *
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None, cost=0):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+        self.cost = cost
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+    def __lt__(self, other):
+        return self.f < other.f
 
 
-def heuristic(a, b):
-    return (b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2
+def astar(maze, start, end):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
 
+    # Initialize both open and closed list
+    open_list = []
+    closedDict = {}
 
-def astar(array, start, goal):
-    neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    # Add the start node
+    heappush(open_list,start_node)
+    # Loop until you find the end
+    while open_list:
+        #sleep(0.2)
+        # Get the current node
+        current_node = heappop(open_list)
 
-    close_set = set()
-    came_from = {}
-    gscore = {start: 0}
-    fscore = {start: heuristic(start, goal)}
-    oheap = []
+        # Pop current off open list, add to closed list
+        #closed_list.append(current_node)
+        #print(num_it, current_node.position, current_node.on_list, current_node.g)
+        #current_node.on_list = True
+        closedDict[current_node.position] = 0
+        # print(current_node.h)
+        # Found the goal
+        #print('open list:', [i.position for i in open_list])
+        #print('closed list:',[i.position for i in closed_list])
+        # print(current_node.position, '=', end_node.position)
+        if current_node.position == end_node.position:
+            end_cost = current_node.g
+            path = []
+            current = current_node
+            while current is not None:
+                path.append([tuple(reversed(current.position)), current.cost])
+                current = current.parent
+            print('returning')
+            return path[::-1], end_cost   # Return reversed path
 
-    heappush(oheap, (fscore[start], start))
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]:  # Adjacent squares
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
-    while oheap:
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (
+                    len(maze[len(maze) - 1]) - 1) or node_position[1] < 0:
+                continue
 
-        current = heappop(oheap)[1]
+            # Make sure walkable terrain
+            node_value = maze[node_position[0]][node_position[1]]
+            if node_value == np.inf:
+                continue
 
-        if current == goal:
-            data = []
-            while current in came_from:
-                data.append(current)
-                current = came_from[current]
-            return data[::-1]
+            # Create new node
+            new_node = Node(current_node, node_position, node_value)
 
-        close_set.add(current)
-        for i, j in neighbors:
-            neighbor = current[0] + i, current[1] + j
-            tentative_g_score = gscore[current] + array[current[0]][current[1]]
-            if 0 <= neighbor[0] < array.shape[0]:
-                if 0 <= neighbor[1] < array.shape[1]:
-                    if array[neighbor[0]][neighbor[1]] == numpy.inf:
-                        continue
-                else:
-                    # array bound y walls
+            # Append
+            children.append(new_node)
+        # Loop through children
+        for child in children:
+            cont = False
+            # Child is on the closed list
+            '''for closed_child in closed_list:
+                if child.position == closed_child.position:
+                    cont = True
                     continue
-            else:
-                # array bound x walls
+            if cont:
+                continue'''
+
+            #if child.on_list:
+             #   continue
+
+            if child.position in closedDict.keys():
                 continue
 
-            if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
+            # Create the f, g, and h values
+            child.g = current_node.g + child.cost
+            child.h = (((child.position[0] - end_node.position[0]))**2 + (
+                        (child.position[1] - end_node.position[1]))**2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    cont = True
+
+                    continue
+            if cont:
                 continue
-
-            if tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1] for i in oheap]:
-                came_from[neighbor] = current
-                gscore[neighbor] = tentative_g_score
-                fscore[neighbor] = tentative_g_score + heuristic(neighbor, goal)
-                heappush(oheap, (fscore[neighbor], neighbor))
-
-    return False
+            # Add the child to the open list
+            heappush(open_list, child)
 
 
-'''Here is an example of using my algo with a numpy array,
-   astar(array, start, destination)
-   astar function returns a list of points (shortest path)'''
+def main():
+    maze = [[0.0, 0.4, np.inf, 0.4, 0.4, 0.2],
+            [0.1, 0.1, np.inf, 0.6, 0.6, 0.2],
+            [np.inf, 0.0, np.inf, 0.8, 0.7, 0.5],
+            [0.7, 0.8, np.inf, 0.9, 0.8, 0.6],
+            [0.7, np.inf, 0.8, 0.8, np.inf, 0.7],
+            [0.5, 0.6, 0.6, np.inf, 0.4, 0.0]]
 
-nmap = numpy.array([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+    start = (0, 0)
+    end = (1, 0)
 
-print(astar(nmap, (0, 0), (10, 13)))
+    path = astar(maze, start, end)
+    print(path)
+
+
+if __name__ == '__main__':
+    main()
